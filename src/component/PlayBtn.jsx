4ba6d7e384ad4bda1081263548647e9e7f13a3b8
rmadbstjd <rmadbstjd@naver.com>
2022-10-31 20:52:39 +0900

    가위,바위,보 이미지를 누르면 handle 함수 실행

--- src/component/PlayBtn.jsx
+++ src/component/PlayBtn.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import styles from './PlayBtn.module.css'
+import {FaHandRock, FaHandScissors, FaRegHandPaper} from "react-icons/fa";
+
+function play(element) {
+
+    let rand = Math.random();
+    let result = Math.floor(rand*3);
+    if(result === 0) {
+        result = 'rock';
+    }
+    else if(result === 1) {
+        result = 'scissors';
+    }
+    else {
+        result ='paper';
+    }
+
+   
+}
+
 function PlayBtn()  {
+    const handleRock = () => {
+        
+        play('rock');
+    }
+    const handleScissors = () => {
+        
+        play('scissors');
+    }
+    const handlePaper = () => {
+        
+        play('paper');
+    }
     return (
         <div className={styles.btn}>
-            test
+            <FaHandRock size="27" onClick={handleRock}></FaHandRock>
+            <FaHandScissors size="27" onClick={handleScissors}/>
+            <FaRegHandPaper size="27" onClick={handlePaper}/>
+            
         </div>
     );
 };
